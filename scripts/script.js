07a4d135be5f726188284fff41a7ac52cd98469b
Brian Bassett <brianbassett@Brians-MBP.lan>
2022-02-14 19:11:47 -0500

    created an array to place the values pulled from the API in, added function to loop through the array and append any defined/truthy information to the DOM, (this is not yet working), added DOM elements to generate/change inner text and append API results to the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const baseURL = "https://api.openbrewerydb.org/breweries?by_city=";
+const baseURL = "https://api.openbrewerydb.org/breweries?per_page=10&by_city=";
 const form = document.querySelector(".cta__form");
 
 const submitHandler = (input) => {
@@ -14,7 +14,48 @@ const submitHandler = (input) => {
   axios
     .get(formattedRequest)
     .then((el) => {
-      console.log(el.data[3].name);
+      console.log(el.data.length);
+      for (i = 0; i < el.data.length; i++) {
+        const arr = [];
+
+        const name = el.data[i].name;
+        const address = el.data[i].address;
+        const phone = el.data[i].phone;
+        const website = el.data[i].website_url;
+
+        arr.push(name, address, phone, website);
+        console.log(arr);
+
+        const listEl = document.createElement("ul");
+
+        function checkEmpty(array) {
+          for (i = 0; i < array.length; i++) {
+            if (array[i]) {
+              const listItem = document.createElement("li");
+              listItem.innerText = array[i];
+              listEl.appendChild(listItem);
+            }
+          }
+
+          // const nameEl = document.createElement("li");
+          // const addressEl = document.createElement("li");
+          // const phoneEl = document.createElement("li");
+          // const websiteEl = document.createElement("li");
+
+          // nameEl.innerText = name;
+          // addressEl.innerText = address;
+          // phoneEl.innerText = phone;
+          // websiteEl.innerText = website;
+
+          // listEl.appendChild(nameEl);
+          // listEl.appendChild(addressEl);
+          // listEl.appendChild(phoneEl);
+          // listEl.appendChild(websiteEl);
+
+          const cardContainer = document.querySelector(".card-container");
+          cardContainer.appendChild(listEl);
+        }
+      }
     })
     .catch((err) => {
       console.log(err);
